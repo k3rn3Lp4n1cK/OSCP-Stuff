4bd60a4aee0ec6d3c57a391ad6ba4666bef63f47
k3rn3Lp4n1cK <huffmanj573@gmail.com>
2023-03-20 16:07:13 -0400

    fix colors; bin softlink

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,57 +1,67 @@
 #!/bin/bash
 
+
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;35m'
 NC='\033[0m'
 
+if [ "$EUID" -ne 0 ]
+    then echo -e "${RED}sudo make me a sandwich!${NC}"
+    exit
+fi
 
 echo
 echo -e "${BLUE}OSCP Kali - Setup Script${NC}"
 apt update -y
 apt upgrade -y
+echo -e "${GREEN}[+] Update/Upgrade Complete${NC}"
 
 
 ######### PACKAGES ##########
 echo
-echo -e "${RED}Packages${NC} Installing common packages"
+echo -e "${YELLOW}Packages${NC} Installing common packages"
 apt install -y ltrace strace edb-debugger wireshark elfutils tldr steghide feroxbuster gobuster terminator gdb checksec default-jdk wireless-tools iw aircrack-ng responder openvpn gcc-multilib
+echo -e "${GREEN}[+] Packages Complete${NC}"
 
 
 ######### TOOLS ##########
 echo
-echo -e "${RED}Tools${NC} Setting up Tool Directories"
+echo -e "${YELLOW}Tools${NC} Setting up Tool Directories"
 mkdir /tools
 cp -r ./tools /
 chown -R kali: /tools
 
 echo
-echo -e "${RED}Softlink Tools${NC} Creating Softlink to Tools bin directory"
+echo -e "${YELLOW}[+] Softlink Tools${NC} Creating Softlink to Tools bin directory"
 ln -s /tools/bin /home/kali/bin
-
+chown kali: /home/kali/bin
+echo -e "${GREEN}[+] Tools Complete${NC}"
 
 ######### GDB PEDA ###########
 echo
-echo -e "${RED}PEDA Setup${NC} Setting up PEDA"
+echo -e "${YELLOW}PEDA Setup${NC} Setting up PEDA"
 cp -r ./peda /opt
 
 echo
-echo -e "${RED}GDB Init${NC} Setting up gdbinit file"
+echo -e "${YELLOW}GDB Init${NC} Setting up gdbinit file"
 cat <<EOF > /home/kali/.gdbinit
 source /opt/peda/peda.py
 EOF
+echo -e "${GREEN}[+] GDB PEDA Complete${NC}"
 
 
 ######### GHIDRA 10.2.2 ##########
 echo
-echo -e "${RED}GHIDRA Setup${NC} Setting up GHIDRA"
+echo -e "${YELLOW}GHIDRA Setup${NC} Setting up GHIDRA"
 cp -r ./ghidra_10.2.2_PUBLIC /opt
+echo -e "${GREEN}[+] Ghidra Complete${NC}"
 
 
 ######### Desktop Icons ##########
 echo
-echo -e "${RED}Desktop Icons${NC} Creating Desktop Shortcut Icons"
+echo -e "${YELLOW}Desktop Icons${NC} Creating Desktop Shortcut Icons"
 cat <<EOF > /home/kali/Desktop/burpsuite.desktop
 [Desktop Entry]
 Version=1.0
@@ -121,8 +131,10 @@ gio set -t string /home/kali/Desktop/ghidra.desktop metadata::xfce-exe-checksum 
 gio set -t string /home/kali/Desktop/Terminator.desktop metadata::xfce-exe-checksum "$(sha256sum /home/kali/Desktop/Terminator.desktop | awk '{print $1}')"
 gio set -t string /home/kali/Desktop/wireshark.desktop metadata::xfce-exe-checksum "$(sha256sum /home/kali/Desktop/wireshark.desktop | awk '{print $1}')"
 
+echo -e "${GREEN}[+] Desktop Icons Complete${NC}"
 
 ########### CLEANUP ###########
 echo
-echo -e "${RED}Cleanup${NC} Cleaning up apt"
+echo -e "${YELLOW}[+] Cleanup${NC} Cleaning up apt"
 apt -y autoremove && sudo apt -y clean all
+echo -e "${GREEN}[+] Cleanup Complete${NC}"
